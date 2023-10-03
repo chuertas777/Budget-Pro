887ca555a96110f151cf2dc2f341a70ae6b1a004
QuinSDev <quindev97@gmail.com>
2023-10-03 17:42:09 -0500

    Se sube la base del proyecto Budget Pro a Github

---
+++ src/main/java/com/budget/budgetpro/BudgetProApplication.java
@@ -0,0 +1,13 @@
+package com.budget.budgetpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/budget/budgetpro/BudgetProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budget.budgetpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
