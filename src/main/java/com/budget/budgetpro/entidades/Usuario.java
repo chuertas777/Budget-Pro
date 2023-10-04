1f1e0a3687985376793e50e1732476fb3e7f0348
QuinSDev <quindev97@gmail.com>
2023-10-03 20:26:23 -0500

    Se crea el paquete entidades, y la entidad Usuario

---
+++ src/main/java/com/budget/budgetpro/entidades/Usuario.java
@@ -0,0 +1,33 @@
+package com.budget.budgetpro.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Getter @Setter
+    private String id;
+    
+    @Getter @Setter
+    private String nombre;
+    
+    @Getter @Setter
+    private String email;
+    
+    @Getter @Setter
+    private String password;
+
+    public Usuario() {
+    }
+    
+    
+    
+}
