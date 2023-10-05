9ccb8ec30bd946f45f69a9ee57eb337b078817f4
QuinSDev <quindev97@gmail.com>
2023-10-05 18:20:47 -0500

    Se agrega la anotación '@ComponentScan' para registrar la CorsConfiguration como una configuración en la aplicación

--- src/main/java/com/budget/budgetpro/BudgetProApplication.java
+++ src/main/java/com/budget/budgetpro/BudgetProApplication.java
@@ -2,8 +2,10 @@ package com.budget.budgetpro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.budget.budgetpro.configuracion")
 public class BudgetProApplication {
 
 	public static void main(String[] args) {
