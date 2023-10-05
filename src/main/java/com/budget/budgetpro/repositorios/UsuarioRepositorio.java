bc44e69de016f0cb4ba328f956c002e79bee04bf
QuinSDev <quindev97@gmail.com>
2023-10-05 18:23:09 -0500

    En el commit anterior se habia creado era el paquete pero no se habia guardado el repositorio del usuario, acá si se cumple con ese commit

--- src/main/java/com/budget/budgetpro/repositorios/UsuarioRepositorio.java
+++ src/main/java/com/budget/budgetpro/repositorios/UsuarioRepositorio.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.budget.budgetpro.repositorios;
 
-/**
- *
- * @author QuinSDev
- */
-public class UsuarioRepositorio {
+import com.budget.budgetpro.entidades.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepositorio extends JpaRepository<Usuario, String>{
     
 }
